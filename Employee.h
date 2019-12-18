75a359fe0b739d88aea47b2d967a1b462eaf3ea7
farucis <*faruch12@gmail.com*>
2019-12-18 16:41:06 +0200

    add after update

---
+++ Employee.h
@@ -0,0 +1,15 @@
+#ifndef Employee_h
+#define Employee_h
+#include"Person.h"
+
+class Employee :public Person 
+{
+private:
+	float salary;
+public:
+	Employee(string _Name, long _ID, int _Age, float _salary) :Person::Person(_Name, _ID, _Age), salary(_salary){
+
+	};
+};
+
+#endif //Employee_H
