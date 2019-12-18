2f5b025f7c813d2ecbec4706e176aa6d0c045463
idanbn <58389697+idanbn@users.noreply.github.com>
2019-12-18 15:19:08 +0200

    Create Employee.h

---
+++ Employee.h
@@ -0,0 +1,21 @@
+#define CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+
+
+class Employee //**public Person**// 
+{
+private:
+	float salary;
+	int age;
+	long ID;
+	char name[10];
+
+public:
+	Student(int c = age, long id = ID, char na[10] = name) {};
+
+
+
+
+};
